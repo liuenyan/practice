48ab5e951f336a621c7598938f2dc446c3795dbd
Enyan Liu <liu.enyan@gmail.com>
2016-11-14 17:13:56 +0800

    translate tabs to spaces

--- String/String.cc
+++ String/String.cc
@@ -1,53 +1,53 @@
 #include "String.h"
 
 namespace enyan{
-	void String::duplicate(const char *cp){
-		if (cp){
-			s = new char[strlen(cp)+1]();
-			strcpy(s, cp);
-		}
-		else{
-			// 当 cp 为空时，直接指向长度为 0 的 C 字符串
-			s = new char[1]();
-		}
-	}
+    void String::duplicate(const char *cp){
+        if (cp){
+            s = new char[strlen(cp)+1]();
+            strcpy(s, cp);
+        }
+        else{
+            // 当 cp 为空时，直接指向长度为 0 的 C 字符串
+            s = new char[1]();
+        }
+    }
 
-	String::String(){
-		s = new char[1]();
-	}
+    String::String(){
+        s = new char[1]();
+    }
 
-	String::String(int n, char c){
-		s = new char[n+1]();
-		for(int i=0; i<n; ++i){
-			s[i] = c;
-		}
-	}
+    String::String(int n, char c){
+        s = new char[n+1]();
+        for(int i=0; i<n; ++i){
+            s[i] = c;
+        }
+    }
 
-	String::String(const String &rs) {
-		duplicate(rs.s);
-	}
+    String::String(const String &rs) {
+        duplicate(rs.s);
+    }
 
-	String::String(const char *cp){
-		duplicate(cp);
-	}
+    String::String(const char *cp){
+        duplicate(cp);
+    }
 
-	String& String::operator=(const String &rs){
-		// 传入的参数是自己本身直接返回
-		if (&rs == this)
-			return *this;
+    String& String::operator=(const String &rs){
+        // 传入的参数是自己本身直接返回
+        if (&rs == this)
+            return *this;
 
-		return operator=(rs.s);
-	}
-	String& String::operator=(const char *cp){
-		// 先复制再释放旧内存， 保证在当cp == this->s 时能够正确复制
-		char *old = s;
-		duplicate(cp);
-		delete[] old;
-		return *this;
-	}
+        return operator=(rs.s);
+    }
+    String& String::operator=(const char *cp){
+        // 先复制再释放旧内存， 保证在当cp == this->s 时能够正确复制
+        char *old = s;
+        duplicate(cp);
+        delete[] old;
+        return *this;
+    }
 
-	std::ostream& operator<<(std::ostream &os, String &str){
-		os << str.s;
-		return os;
-	}
+    std::ostream& operator<<(std::ostream &os, String &str){
+        os << str.s;
+        return os;
+    }
 }
--- String/main.cc
+++ String/main.cc
@@ -4,38 +4,38 @@ using enyan::String;
 using namespace std;
 
 int main(){
-	String s1(5, 'c');
-	cout << s1 << endl;
-	cout << s1.size() << endl;
-	String s2 = s1;
-	cout << s2 << endl;
-	cout << s2.size() << endl;
+    String s1(5, 'c');
+    cout << s1 << endl;
+    cout << s1.size() << endl;
+    String s2 = s1;
+    cout << s2 << endl;
+    cout << s2.size() << endl;
 
-	String s3;
-	cout << s3 << endl;
-	cout << s3.size() << endl;
+    String s3;
+    cout << s3 << endl;
+    cout << s3.size() << endl;
 
-	s3 = s2;
-	cout << s3 << endl;
-	cout << s3.size() << endl;
+    s3 = s2;
+    cout << s3 << endl;
+    cout << s3.size() << endl;
 
-	//尝试拷贝自身
-	s3 = s3;
-	cout << s3 << endl;
-	cout << s3.size() << endl;
+    //尝试拷贝自身
+    s3 = s3;
+    cout << s3 << endl;
+    cout << s3.size() << endl;
 
-	const char *cp1 = "test";
-	String s4 = cp1;
+    const char *cp1 = "test";
+    String s4 = cp1;
 
-	cout << s4 << endl;
-	cout << s4.size() << endl;
+    cout << s4 << endl;
+    cout << s4.size() << endl;
 
-	//调用拷贝赋值运算符
-	const char *cp2 = s4.c_str();
-	s4 = cp2;
+    //调用拷贝赋值运算符
+    const char *cp2 = s4.c_str();
+    s4 = cp2;
 
-	cout << s4 << endl;
-	cout << s4.size() << endl;
+    cout << s4 << endl;
+    cout << s4.size() << endl;
 
-	return 0;
+    return 0;
 }
