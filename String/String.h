9b53c7c153d68ff7865633f835b057a071b7ce11
Enyan Liu <liu.enyan@gmail.com>
2016-11-14 12:47:24 +0800

    一个简单的字符串类的实现

---
+++ String/String.h
@@ -0,0 +1,27 @@
+#ifndef _ENYAN_STRING_H_
+#define _ENYAN_STRING_H_
+
+#include <cstring>
+#include <iostream>
+
+namespace enyan{
+	class String{
+		char *s;
+		void duplicate(const char *cp);
+	public:
+		String();
+		String(int n, char c);
+		String(const String &rs); //copy constructor
+		String(const char *cp);
+		~String(){delete[] s;};
+
+		String& operator=(const String &rs);
+		String& operator=(const char *cp);
+
+		size_t size() const {return strlen(s);};
+		const char *c_str() const{return s;};
+		friend std::ostream &operator<<(std::ostream &, String &);
+	};
+};
+
+#endif
---
+++ String/main.cc
@@ -0,0 +1,41 @@
+#include "String.h"
+
+using enyan::String;
+using namespace std;
+
+int main(){
+	String s1(5, 'c');
+	cout << s1 << endl;
+	cout << s1.size() << endl;
+	String s2 = s1;
+	cout << s2 << endl;
+	cout << s2.size() << endl;
+
+	String s3;
+	cout << s3 << endl;
+	cout << s3.size() << endl;
+
+	s3 = s2;
+	cout << s3 << endl;
+	cout << s3.size() << endl;
+
+	//尝试拷贝自身
+	s3 = s3;
+	cout << s3 << endl;
+	cout << s3.size() << endl;
+
+	const char *cp1 = "test";
+	String s4 = cp1;
+
+	cout << s4 << endl;
+	cout << s4.size() << endl;
+
+	//调用拷贝赋值运算符
+	const char *cp2 = s4.c_str();
+	s4 = cp2;
+
+	cout << s4 << endl;
+	cout << s4.size() << endl;
+
+	return 0;
+}
