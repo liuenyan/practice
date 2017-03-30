5d0ac67e93d54bf32ce9fbb3a1bdef276eb85e73
Enyan Liu <liu.enyan@gmail.com>
2017-03-30 09:57:38 +0800

    增加堆排序算法

---
+++ Algorithm/sort/heap_sort.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef int element_t;
+
+void print_array(element_t a[], int size)
+{ 
+    for(int i=0; i<size; i++){
+        printf(" %d", a[i]);
+    }
+    printf("\n");
+}
+
+int swap(element_t elements[], int i, int j)
+{
+    element_t tmp = elements[i];
+    elements[i] = elements[j];
+    elements[j] = tmp;
+}
+
+int max_heapify(element_t elements[], int element_size, int index)
+{
+
+    int largest = index;          /*最大值的下标*/
+    int left_index = 2 * index + 1;     /*左节点*/
+    int right_index = 2 * index + 2;    /*右节点*/
+
+    if(left_index < element_size && elements[largest] < elements[left_index]) {
+        largest = left_index;
+    }
+    if(right_index < element_size && elements[largest] < elements[right_index]) {
+        largest = right_index;
+    }
+
+    if(largest != index) {
+        swap(elements, index, largest);
+        max_heapify(elements, element_size, largest);
+    }
+}
+
+int make_max_heap(element_t elements[], int element_size)
+{
+    for(int i = element_size/2-1; i >= 0; --i){
+        max_heapify(elements, element_size, i);
+    }
+}
+
+int heap_sort(element_t elements[], int element_size) {
+    
+    for(int i=element_size; i>=1; i--) {
+        make_max_heap(elements, i);
+        swap(elements, i-1, 0);
+    }
+}
+
+
+int main()
+{
+    element_t a[21];
+    srand(time(NULL));
+
+    for(int i=0; i<21; i++) {
+        a[i] = rand() % 1000;
+    }
+
+    print_array(a, sizeof(a)/sizeof(element_t));
+    heap_sort(a, sizeof(a)/sizeof(element_t));
+    print_array(a, sizeof(a)/sizeof(element_t));
+    return 0;
+}
+
