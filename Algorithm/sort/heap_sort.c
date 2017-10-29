9ff4caab68677836c5abe28ffe73fcf0dfc6fe4e
Enyan Liu <liu.enyan@gmail.com>
2017-10-29 11:45:23 +0800

    修复堆排序中的问题

--- Algorithm/sort/heap_sort.c
+++ Algorithm/sort/heap_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-static void swap(element_t elements[], int i, int j)
+static void exchange(element_t elements[], int i, int j)
 {
     element_t tmp = elements[i];
     elements[i] = elements[j];
@@ -9,36 +9,35 @@ static void swap(element_t elements[], int i, int j)
 
 static void max_heapify(element_t elements[], int element_size, int index)
 {
-    int largest = index;          /*最大值的下标*/
-    int left_index = 2 * index + 1;     /*左节点*/
-    int right_index = 2 * index + 2;    /*右节点*/
+    int largest = index; /*最大值的下标*/
+    int left_index = 2 * index + 1; /*左节点*/
+    int right_index = 2 * index + 2; /*右节点*/
 
-    if(left_index < element_size && elements[largest] < elements[left_index]) {
+    if (left_index < element_size && elements[largest] < elements[left_index]) {
         largest = left_index;
     }
-    if(right_index < element_size && elements[largest] < elements[right_index]) {
+    if (right_index < element_size && elements[largest] < elements[right_index]) {
         largest = right_index;
     }
 
-    if(largest != index) {
-        swap(elements, index, largest);
+    if (largest != index) {
+        exchange(elements, index, largest);
         max_heapify(elements, element_size, largest);
     }
 }
 
 static void make_max_heap(element_t elements[], int element_size)
 {
-    for(int i = element_size/2-1; i >= 0; --i) {
+    for (int i = element_size / 2 - 1; i >= 0; --i) {
         max_heapify(elements, element_size, i);
     }
 }
 
 void heap_sort(element_t elements[], int element_size)
 {
-
-    for(int i=element_size; i>=1; i--) {
-        make_max_heap(elements, i);
-        swap(elements, i-1, 0);
+    make_max_heap(elements, element_size);
+    for (int i = element_size; i >= 2; i--) {
+        exchange(elements, 0, i - 1);
+        max_heapify(elements, i - 1, 0);
     }
 }
-
